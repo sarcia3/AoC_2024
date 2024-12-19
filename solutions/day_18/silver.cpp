09142cf6cf0b7188eca3bb5c300ab0689220a8c0
Sara Bartman <sara.bartman.05@gmail.com>
2024-12-19 11:21:08 +0100

    Day 18 solutions

---
+++ solutions/day_18/silver.cpp
@@ -0,0 +1,71 @@
+#include "bits/stdc++.h"
+#define int long long
+#define pi pair<long long, long long>
+#define a3 array<long long, 3>
+
+#define meow main
+
+using namespace std;
+
+const int n = 71;
+signed meow() {
+    vector<vector<int>> G(71, vector<int> (71));
+    int tmp = 1024;
+    while(tmp--){
+        string s; cin >> s;
+        int num=0;
+        int i;
+        for(i=0; s[i] != ','; i++){
+            num*=10;
+            num+=s[i]-'0';
+        }
+        int num2=0;
+        i++;
+        for(; i <s.size(); i++){
+            num2*=10;
+            num2+=s[i]-'0';
+        }
+        G[num2][num] = true;
+    }
+
+    queue<pair<pi, int>> Q;
+    vector<vector<bool>> vis(n, vector<bool> (n));
+
+    auto is_legal = [&](pi arg){
+        return (arg.first>=0 && arg.first < 71 && 
+        arg.second>=0 && arg.second < 71 &&
+        !G[arg.first][arg.second] && !vis[arg.first][arg.second]);
+    };
+
+    Q.push({{0, 0}, 0});
+    vector<vector<int>> dist(71, vector<int> (71, INT_MAX));
+
+    while(Q.size()){
+        auto [p, d] = Q.front();
+        auto [x, y] = p;
+        Q.pop();
+        if(dist[x][y] <= d) continue;
+        dist[x][y] = d;
+        vis[x][y] = true;
+        if(x==n-1&& y==n-1) break ;
+        if(is_legal({x+1, y})){
+            Q.push({{x+1, y},d+1});
+           // dist[x+1][y] = d+1;
+        }
+        if(is_legal({x, y+1})){
+            Q.push({{x, y+1},d+1});
+           // dist[x][y+1] = d+1;
+        }
+        if(is_legal({x-1, y})){
+            Q.push({{x-1, y},d+1});
+          //  dist[x-1][y] = d+1;
+        }
+        if(is_legal({x, y-1})){
+            Q.push({{x, y-1},d+1});
+          //  dist[x][y-1] = d+1;
+        }
+
+    }
+
+    cout << dist[n-1][n-1];
+}
